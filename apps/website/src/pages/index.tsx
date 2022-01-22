899775355617c8ee4d7358d442522b6317ee284c
Faiz Maricar <maricar.faiz@gmail.com>
2022-01-22 15:08:36 +0800

    created basic home page structure

--- apps/website/src/pages/index.tsx
+++ apps/website/src/pages/index.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
-import styled from '@emotion/styled';
-
-const StyledApp = styled.div``;
+import { StaticImage } from 'gatsby-plugin-image';
 
 export function Index() {
-  return <h1>Faiz Maricar</h1>;
+  return (
+    <>
+      <h1>Faiz Maricar</h1>
+      <h2>About me</h2>
+      <StaticImage src="../images/profile.jpg" alt="profile" />
+      <p>
+        I'm a software engineer for the past 5 years. I specialize in web and
+        mobile development.
+      </p>
+    </>
+  );
 }
 
 export default Index;
--- libs/react-ui/src/lib/global-styles/global-styles.tsx
+++ libs/react-ui/src/lib/global-styles/global-styles.tsx
@@ -4,9 +4,23 @@ export function GlobalStyles() {
   return (
     <Global
       styles={css`
+        @import url('https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap');
         * {
           margin: 0;
           padding: 0;
+        }
+
+        h1,
+        h2,
+        h3,
+        h4,
+        h5,
+        h6 {
+          font-family: 'Space Mono', monospace;
+        }
+
+        p {
+          font-family: 'Roboto Mono', monospace;
         }
       `}
     />
